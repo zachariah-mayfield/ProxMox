ae54ef832a79236432de89a60a1ef3fc3948dca4
Zachariah-Mayfield <zachariah-mayfield@outlook.com>
2025-07-15 23:39:13 -0500

    added @pytest.fixture

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,3 +2,9 @@ def pytest_addoption(parser):
     parser.addoption(
         "--ssh-key", action="store", default=None, help="Path to SSH private key"
     )
+
+import pytest
+
+@pytest.fixture
+def ssh_key(request):
+    return request.config.getoption("--ssh-key")
